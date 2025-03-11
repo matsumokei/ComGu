f8319efaf1b336cddca5c0079dbec77c8d2b227f
matsumoto <matsumo162570@gmail.com>
2025-03-11 17:24:00 +0900

    feat: add a createComponent method which create new Component to ComponentServise and Resolver

--- src/module/component/component.resolver.ts
+++ src/module/component/component.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ComponentService } from './component.service';
+import { Component as ComponentModel } from './model/component.model';
+import { Component } from '@prisma/client';
+import { CreateComponentInput } from './dto/createComponent.input';
 
 @Resolver()
-export class ComponentResolver {}
+export class ComponentResolver {
+    constructor(private readonly componentService: ComponentService) {};
+
+    @Query(() => [ComponentModel], { nullable: "items"})
+    async getComponentListById(
+        @Args('id', { type: () => String }) id: string,
+    ): Promise<Component[]> {
+        return await this.componentService.getComponentListById(id);
+    }
+
+    @Mutation(() => ComponentModel)
+    async createComponent(
+        @Args('createComponent') createComponentInput: CreateComponentInput
+    ): Promise<Component> {
+        return await this.componentService.createComponent(createComponentInput);
+    }
+}
--- src/module/component/component.service.ts
+++ src/module/component/component.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Component } from '@prisma/client';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+import { CreateComponentInput } from './dto/createComponent.input';
 
 @Injectable()
-export class ComponentService {}
+export class ComponentService {
+    constructor(private readonly prisma: PrismaService) {}
+    
+    async getComponentListById(id: string): Promise<Component[]> {
+        return await this.prisma.component.findMany({
+                where: { id },
+        });
+    }
+
+    async createComponent(createComponentInput: CreateComponentInput): Promise<Component> {
+        const { bomref, purl, name, version, vulns_id, fixed_version } = createComponentInput
+        return await this.prisma.component.create({
+            data: { bomref, purl, name, version, vulns_id, fixed_version }
+        });
+    }
+}
